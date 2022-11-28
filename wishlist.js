07b7f1fc69024445889ad1485be796ebaf772179
alixrayann <119272966+alixrayann@users.noreply.github.com>
2022-11-27 19:40:29 -0700

    Add files via upload

---
+++ wishlist.js
@@ -0,0 +1,18 @@
+'use strict';
+setup() {
+}
+
+const cart = [ ]
+
+function addItem(name, price) {
+cart.push(name)
+}
+
+function showItems() {
+console.log(cart)
+}
+
+addItem('dogs', $50-$150)
+addItem('cats', $20-$80)
+
+showItems()
